b6ffef415243c387185f4f848180a1ffa11be1ca
Thiago <thiago.pereira@ontariotechu.net>
2024-02-21 14:10:37 -0500

    changes

--- scripts/ignore-build.mjs
+++ scripts/ignore-build.mjs
@@ -1,26 +1,42 @@
 import https from "node:https";
 
 // dumb fetch implementation
-const fetch = (url, options = {}) => new Promise((resolve, reject) => {
-  const req = https.request(url, options, (res) => {
-    if (res.statusCode < 200 || res.statusCode >= 300) {
-      return reject(new Error('statusCode=' + res.statusCode));
-    }
-    let chunks = [];
-    res.on("data", (chunk) => { chunks.push(chunk) });
-    res.on("end", () => resolve({ json: () => JSON.parse(Buffer.concat(chunks).toString()) }));
-  }).on("error", reject);
-  req.end();
-});
+const fetch = (url, options = {}) =>
+  new Promise((resolve, reject) => {
+    const req = https
+      .request(url, options, (res) => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          return reject(new Error("statusCode=" + res.statusCode));
+        }
+        let chunks = [];
+        res.on("data", (chunk) => {
+          chunks.push(chunk);
+        });
+        res.on("end", () =>
+          resolve({ json: () => JSON.parse(Buffer.concat(chunks).toString()) }),
+        );
+      })
+      .on("error", reject);
+    req.end();
+  });
 
 if (process.env.VERCEL_ENV === "preview") {
-  const teamId = process.env.VERCEL_TEAM_ID ? `?teamId=${process.env.VERCEL_TEAM_ID}` : '';
-  const deployment = await fetch(`https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}${teamId}`, {
-    headers: { Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` }
-  }).then(res => res.json());
-  if (!deployment.name.startsWith('snaplet-action-')) {
+  const teamId = process.env.VERCEL_TEAM_ID
+    ? `?teamId=${process.env.VERCEL_TEAM_ID}`
+    : "";
+  const deployment = await fetch(
+    `https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}${teamId}`,
+    {
+      headers: { Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` },
+    },
+  ).then((res) => res.json());
+  if (
+    !deployment.name.startsWith("snaplet-action-") ||
+    !process.env.NEXT_PUBLIC_URL
+  ) {
     process.exit(0);
   }
 }
 
 process.exit(1);
+
