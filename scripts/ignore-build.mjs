818f221fa8965d6cafb99e51b6fbe2a4d5a94fb8
Thiago <thiago.pereira@ontariotechu.net>
2024-02-28 08:58:04 -0500

    fix

--- scripts/ignore-build.mjs
+++ scripts/ignore-build.mjs
@@ -31,7 +31,10 @@ if (process.env.VERCEL_ENV === "preview") {
     },
   ).then((res) => res.json());
   const branch = deployment.meta.gitBranch;
-  if (!deployment.name.startsWith("snaplet-action-") && branch !== "staging") {
+  if (
+    !deployment.name.startsWith("snaplet-action-") &&
+    !process.env.NEXT_PUBLIC_URL
+  ) {
     process.exit(0);
   }
 }
