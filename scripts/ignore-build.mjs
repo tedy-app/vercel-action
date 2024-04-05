83cfd003d40babd01f55cb5c04c552faee9a558d
Thiagoxp95 <62924127+Thiagoxp95@users.noreply.github.com>
2024-04-05 16:49:31 -0400

    Update ignore-build.mjs

--- scripts/ignore-build.mjs
+++ scripts/ignore-build.mjs
@@ -1,38 +1,30 @@
 import https from "node:https";
 
 // dumb fetch implementation
-const fetch = (url, options = {}) =>
-  new Promise((resolve, reject) => {
-    const req = https
-      .request(url, options, (res) => {
-        if (res.statusCode < 200 || res.statusCode >= 300) {
-          return reject(new Error("statusCode=" + res.statusCode));
-        }
-        let chunks = [];
-        res.on("data", (chunk) => {
-          chunks.push(chunk);
-        });
-        res.on("end", () =>
-          resolve({ json: () => JSON.parse(Buffer.concat(chunks).toString()) }),
-        );
-      })
-      .on("error", reject);
-    req.end();
-  });
+const fetch = (url, options = {}) => new Promise((resolve, reject) => {
+  const req = https.request(url, options, (res) => {
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      return reject(new Error('statusCode=' + res.statusCode));
+    }
+    let chunks = [];
+    res.on("data", (chunk) => { chunks.push(chunk) });
+    res.on("end", () => resolve({ json: () => JSON.parse(Buffer.concat(chunks).toString()) }));
+  }).on("error", reject);
+  req.end();
+});
 
 if (process.env.VERCEL_ENV === "preview") {
-  const teamId = process.env.VERCEL_TEAM_ID
-    ? `?teamId=${process.env.VERCEL_TEAM_ID}`
-    : "";
-  const deployment = await fetch(
-    `https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}${teamId}`,
-    {
-      headers: { Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` },
-    },
-  ).then((res) => res.json());
-  const isNotSnapletGenerated = !deployment.name.startsWith("snaplet-action-");
-  const isNotStaging = deployment.data?.gitSource?.ref !== 'staging'
-  if (isNotSnapletGenerated && isNotStaging )) {
+  const teamId = process.env.VERCEL_TEAM_ID ? `?teamId=${process.env.VERCEL_TEAM_ID}` : '';
+  const deployment = await fetch(`https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}${teamId}`, {
+    headers: { Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}` }
+  }).then(res => res.json());
+  const isSnapletDeployment = deployment.name.startsWith('snaplet-action-');
+  const isStaging = deployment.data?.gitSource?.ref === 'staging'
+  
+  if (isStaging) {
+    process.exit(1)
+  }
+  if (!isSnapletDeployment) {
     process.exit(0);
   }
 }
